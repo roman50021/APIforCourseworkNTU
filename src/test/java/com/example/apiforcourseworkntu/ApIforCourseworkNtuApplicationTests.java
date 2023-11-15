d11a825c7fc3d7da9858ff42fa06e15d9dac426f
“Roman <fedkoroma2@gmail.comgit config --global user.email fedkoroma2@gmail.com>
2023-11-15 20:26:31 +0200

    Configuration

---
+++ src/test/java/com/example/apiforcourseworkntu/ApIforCourseworkNtuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiforcourseworkntu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIforCourseworkNtuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
