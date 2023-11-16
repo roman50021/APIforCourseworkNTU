0ab13771c99856a6356babdf8e03a951dbb41cfd
“Roman <fedkoroma2@gmail.comgit config --global user.email fedkoroma2@gmail.com>
2023-11-16 20:43:39 +0200

    JWT Authentication and Authorisation

--- src/main/java/com/example/apiforcourseworkntu/ApIforCourseworkNtuApplication.java
+++ src/main/java/com/example/apiforcourseworkntu/ApIforCourseworkNtuApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApIforCourseworkNtuApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApIforCourseworkNtuApplication.class, args);
     }
---
+++ src/main/java/com/example/apiforcourseworkntu/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.apiforcourseworkntu.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
